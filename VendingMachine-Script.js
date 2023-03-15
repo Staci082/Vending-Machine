5ea9c40ee41cb1bf60167a422f504d842b1098b2
Staci082 <125351559+Staci082@users.noreply.github.com>
2023-03-15 22:00:22 +0100

    UPDATE

---
+++ VendingMachine-Script.js
@@ -0,0 +1,22 @@
+/* images = {
+    "1" : "lime.png",
+    "2" : "pineapple.png",
+    "3" : "cola.png",
+    "4" : "energy.png",
+    "5" : "jungle.png",
+    "6" : "coco.png",
+    "7" : "mystery.png",
+    "8" : "redbar.png",
+    "9" : "yellowbar.png",
+    "10" : "pinkbar.png",
+    "11" : "orange.png",
+    "12" : "lemon.png",
+    "13" : "tropical.png",
+    "14" : "brownbar.png",
+    "15" : "redchips.png",
+    "16" : "yellowchips.png",
+    "17" : "greenchips.png",
+    "18" : "pinkchips.png",
+ }
+*/
+
