b8fa9008839bb5964306646e1723921efdd46e95
Staci082 <125351559+Staci082@users.noreply.github.com>
2023-03-16 22:54:45 +0100

    comments added

--- VendingMachine-Script.js
+++ VendingMachine-Script.js
@@ -1,26 +1,15 @@
 // variables
 
 
- /*
-document.getElementById('A').value 
-document.getElementById('B').value 
-document.getElementById('C').value 
-document.getElementById('D').value 
-document.getElementById('E').value 
-document.getElementById('1').value 
-document.getElementById('2').value 
-document.getElementById('3').value
-document.getElementById('4').value 
-document.getElementById('5').value 
-*/
 
 // event listeners
 
-// btn.addEventListener("click", getChoice, true);
 
 
 // functions
 
+
+// gets buttons to output their value to textbox
 document.addEventListener('DOMContentLoaded', function() {
    let textbox = document.querySelector("#textInput");
    let buttons = document.querySelectorAll(".btn");
