fb35387dd96f72b84e3b85e2796082cbc6038e20
Staci082 <sjjohnson082@gmail.com>
2023-03-18 17:31:42 +0100

    update

--- VendingMachine-Script.js
+++ VendingMachine-Script.js
@@ -13,21 +13,25 @@ document.addEventListener('DOMContentLoaded', function() {
    });
  });
 
+
+ // puts value's into specified strings 
+
  function getValue() {
    let input = document.getElementsByTagName("input")[0];
+   let textbox =  document.getElementById("makeChoice");
 
    switch (true) {
-      case input.value === "B1":
-         alert("lime soda");
-         break;
+      case (input.value) === "B1": 
+         textbox.innerHTML = "lime soda";
+      break;
 
       default: 
-      alert("error");
+      alert("out of stock");
       break;
-   }
-
+ }
  }
 
+ 
 /* function scrollUp()
 {
    menuCont.scrollBy(0,20);
