bd4f6f7bfbb718ac89b9baa7f4bab319323aaced
Staci082 <sjjohnson082@gmail.com>
2023-03-18 17:00:11 +0100

    lime soda output working

--- VendingMachine-Script.js
+++ VendingMachine-Script.js
@@ -8,18 +8,27 @@ document.addEventListener('DOMContentLoaded', function() {
    
    buttons.forEach(function(btn) {
      btn.addEventListener("click", function(e) {
-       textbox.value += this.value + " ";
+       textbox.value += this.value;
      });
    });
  });
 
  function getValue() {
    let input = document.getElementsByTagName("input")[0];
-   alert(input.value);
+
+   switch (true) {
+      case input.value === "B1":
+         alert("lime soda");
+         break;
+
+      default: 
+      alert("error");
+      break;
+   }
+
  }
 
-
-function scrollUp()
+/* function scrollUp()
 {
    menuCont.scrollBy(0,20);
 }
@@ -28,6 +37,7 @@ function scrollDown()
 {
    menuCont.scrollBy(0,-20);
 }
+*/
 
 $('#delete').click(function() {
    location.reload(true);
