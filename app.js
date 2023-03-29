4e2e4652df2ad0bd5b3224454a7e8a60738d5849
Staci082 <125351559+Staci082@users.noreply.github.com>
2023-03-29 21:33:24 +0200

    files sorted

---
+++ app.js
@@ -0,0 +1,81 @@
+
+
+// file connections
+
+async function IncludeJavascript(filename){
+  var script = await document.createElement('script');
+  // <script src=""
+  script.src = await filename;
+  return new Promise((resolve,reject)=>{
+      script.onload = ()=>{
+          resolve()
+      }
+      document.body.appendChild(script);
+  })
+}
+
+IncludeJavascript("menu.js") // import µ
+IncludeJavascript("textBtns.js") // import 
+IncludeJavascript("variables.js") // import
+
+window.onload = ()=>{ // html event
+    
+  console.log(message)
+  console.log(GetName('John Doe'))
+  console.log(menu)
+
+}
+
+
+
+// gets buttons to output their value to text container
+
+document.addEventListener("DOMContentLoaded", function () {
+  let textbox = document.querySelector("#textInput");
+  let buttons = document.querySelectorAll(".btn");
+
+  buttons.forEach(function (btn) {
+    btn.addEventListener("click", function () {
+      textbox.value += this.value;
+    });
+  });
+});
+
+
+// enables number buttons after letter button is clicked
+
+function enabler() {
+  var x = document.getElementsByClassName("number");
+  var i;
+  for (i = 0; i < x.length; i++) {
+    x[i].disabled = false;
+  }
+}
+  
+
+// puts value's into specified strings
+
+  function getValue(event) {
+  let input = document.getElementsByTagName("input")[0];
+
+  event.preventDefault();
+  $("#textInput").hide();
+
+  let match = (drinks.find(element => (element.code === input.value)))
+
+
+  if (input.value === match.code); `${(match.name)}`;
+
+  text.innerHTML = `You have selected: <br/> ${(match.name) + " :"}  <br/>     ${(match.price) + " euro"}`;
+}
+// else {
+//   output.innerHTML = "Out of stock" ;
+
+
+
+
+// reloads text
+
+$("#delete").click(function () {
+  location.reload(true);
+});
---
+++ menu.js
@@ -0,0 +1,44 @@
+
+
+// item description on hover toggle
+
+$("#item1").hover(
+    function () {
+      $("#des1").show();
+    },
+    function () {
+      $("#des1").hide();
+    }
+  );
+  $("#item2").hover(
+    function () {
+      $("#des2").show();
+    },
+    function () {
+      $("#des2").hide();
+    }
+  );
+  $("#item3").hover(
+    function () {
+      $("#des3").show();
+    },
+    function () {
+      $("#des3").hide();
+    }
+  );
+  $("#item4").hover(
+    function () {
+      $("#des4").show();
+    },
+    function () {
+      $("#des4").hide();
+    }
+  );
+  $("#item5").hover(
+    function () {
+      $("#des5").show();
+    },
+    function () {
+      $("#des5").hide();
+    }
+  );
