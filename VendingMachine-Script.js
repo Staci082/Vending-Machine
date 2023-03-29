f81e641c96dd633e376a18b18697587fa48055d8
Staci082 <125351559+Staci082@users.noreply.github.com>
2023-03-29 19:22:19 +0200

    update

--- VendingMachine-Script.js
+++ VendingMachine-Script.js
@@ -4,27 +4,32 @@ const text = document.querySelector("#textBox");
 const drinks = [{
     name: "lime soda",
     price: 1.5,
-    code: "B1"
+    code: "B1",
+    img: "./images/lime.png"
   },
   {
     name: "pineapple soda",
     price: 1.5,
-    code: "B2"
+    code: "B2",
+    img: "./images/pineapple.png"
   },
   {
     name: "cola drink",
     price: 1.5,
-    code: "B3"
+    code: "B3",
+    img: "./images/cola.png"
   },
   {
     name: "energy drink",
     price: 2.0,
-    code: "B4"
+    code: "B4",
+    img: "./images/energy.png"
   },
   {
     name: "jungle drink",
     price: 2.0,
-    code: "B5"
+    code: "B5",
+    img: "./images/jungle.png"
   },
 ]
 
