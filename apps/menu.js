3d4df1ae7bed4a58ebd45d7cbe7d8411e1b2da3c
Staci082 <125351559+Staci082@users.noreply.github.com>
2023-04-09 18:45:12 +0200

    coins

--- apps/menu.js
+++ apps/menu.js
@@ -1,39 +1,133 @@
 
 const containerD = document.querySelector(".itemContainer");
-const drinks = [
-  {
-    id: 1,
-    src: "./images/lime.png",
-  },
-  {
-    id: 2,
-    src: "./images/pineapple.png",
-  },
-  {
-    id: 3,
-    src: "./images/cola.png",
-  },
-  {
-    id: 4,
-    src: "./images/energy.png",
-  },
-  {
-    id: 5,
-    src: "./images/jungle.png",
-  },
-  {
-    id: 6,
-    src: "./images/mystery.png",
-  },
-];
+const options = {
+  drinks: [
+    {
+      id: 1,
+      name: "lime soda",
+      price: 1.5,
+      code: "B1",
+      src: "./images/lime.png",
+      description: "lime soda &nbsp; € 1.5 &nbsp; B 1",
+    },
+    {
+      id: 2,
+      name: "pineapple soda",
+      price: 1.5,
+      code: "B2",
+      src: "./images/pineapple.png",
+      description: "pineapple soda &nbsp; € 1.5 &nbsp; B 2",
+    },
+    {
+      id: 3,
+      name: "cola drink",
+      price: 1.5,
+      code: "B3",
+      src: "./images/cola.png",
+      description: "cola drink &nbsp; € 1.5 &nbsp; B 3",
+    },
+    {
+      id: 4,
+      name: "energy drink",
+      price: 2.0,
+      code: "B4",
+      src: "./images/energy.png",
+      description: "energy drink &nbsp; € 2.0 &nbsp; B 4",
+    },
+    {
+      id: 5,
+      name: "jungle drink",
+      price: 2.0,
+      code: "B5",
+      src: "./images/jungle.png",
+      description: "jungle drink &nbsp; € 2.0 &nbsp; B 5",
+    },
+    {
+      id: 6,
+      name: "mystery drink",
+      price: 2.0,
+      code: "E2",
+      src: "./images/mystery.png",
+      description: "mystery drink &nbsp; € 2.0 &nbsp; E 2",
+    },
+  ],
+  chocolates: [
+    {
+      id: 7,
+      name: "candy bar",
+      price: 1.5,
+      code: "C1",
+      src: "./images/redbar.png",
+      description: "candy bar &nbsp; € 1.5 &nbsp; C 1",
+    },
+    {
+      id: 8,
+      name: "nutty bar",
+      price: 1.5,
+      code: "C2",
+      src: "./images/yellowbar.png",
+      description: "nutty bar &nbsp; € 1.5 &nbsp; C 2",
+    },
+    {
+      id: 9,
+      name: "fruity bar",
+      price: 1.5,
+      code: "C3",
+      src: "./images/pinkbar.png",
+      description: "fruity bar &nbsp; € 1.5 &nbsp; C 3",
+    },
+    {
+      id: 10,
+      name: "chocolate bar",
+      price: 1.5,
+      code: "C4",
+      src: "./images/brownbar.png",
+      description: "chocolate bar &nbsp; € 1.5 &nbsp; C 4",
+    },
+  ],
+  chips: [
+    {
+      id: 11,
+      name: "salt chips",
+      price: 2.0,
+      code: "A1",
+      src: "./images/redchips.png",
+      description: "salt chips &nbsp; € 2.0 &nbsp; A 1",
+    },
+    {
+      id: 12,
+      name: "tortilla chips",
+      price: 2.0,
+      code: "A2",
+      src: "./images/yellowchips.png",
+      description: "tortilla chips &nbsp; € 2.0 &nbsp; A 2",
+    },
+    {
+      id: 13,
+      name: "paprika chips",
+      price: 2.0,
+      code: "A3",
+      src: "./images/greenchips.png",
+      description: "paprika chips &nbsp; € 2.0 &nbsp; A 3",
+    },
+    {
+      id: 14,
+      name: "cheesy chips",
+      price: 2.0,
+      code: "A4",
+      src: "./images/pinkchips.png",
+      description: "cheesy chips &nbsp; € 2.0 &nbsp; A 4",
+    },
+  ],
+};
 
-drinks.forEach(drink => {
+options.drinks.forEach(item => {
   const div = document.createElement("div");
-  div.className = "drink";
+  div.className = "item";
 
   const img = document.createElement("img");
-  img.src = drink.src;
-
+  img.src = item.src;
+  img.style.height = "14vh";
 
   div.appendChild(img);
 
