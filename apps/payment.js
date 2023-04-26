0a11a0cac5fce652ee4c23f4ae0fdb5cc91f555d
Staci082 <125351559+Staci082@users.noreply.github.com>
2023-04-26 14:26:17 +0200

    coin buttons css

--- apps/payment.js
+++ apps/payment.js
@@ -10,44 +10,6 @@ let currentAmount = 0;
 const closeButton = document.querySelector(".close");
 const reset = document.querySelector("#reset");
 
-const coins = [
-    {
-        id: 15,
-        name: "2euro",
-        value: 2,
-        src: "./images/2euro.png",
-    },
-    {
-        id: 16,
-        name: "1euro",
-        value: 1,
-        src: "./images/1euro.png",
-    },
-    {
-        id: 17,
-        name: "50cent",
-        value: 0.5,
-        src: "./images/50cent.png",
-    },
-    {
-        id: 18,
-        name: "20cent",
-        value: 0.2,
-        src: "./images/20cent.png",
-    },
-    {
-        id: 19,
-        name: "10cent",
-        value: 0.1,
-        src: "./images/10cent.png",
-    },
-    {
-        id: 20,
-        name: "5cent",
-        value: 0.05,
-        src: "./images/5cent.png",
-    },
-];
 
 // payment button
 
